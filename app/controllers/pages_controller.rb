d5503e3b9ad5ae514d7a40a7a7d48eef309c9dd5
Eric Marrone <emarrone17@gmail.com>
2017-11-21 15:33:36 -0500

    Add header instance variable to controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,15 @@
 class PagesController < ApplicationController
 
   def  welcome
-    render :welcome
+    @header = "This is the welcome page"
   end
 
   def about
-
+    @header = "ABOUT"
   end
 
   def contest
+    @header = "Enter the contest"
   end
-  
+
 end
