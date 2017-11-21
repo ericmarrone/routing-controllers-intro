15c5b7b87c0d99da5aa31b0de0855d74d756e978
Eric Marrone <emarrone17@gmail.com>
2017-11-21 16:28:07 -0500

    Replace url with URL helpers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ class PagesController < ApplicationController
 
   def contest
     flash[:notice] = "Sorry, the contest has ended"
-    redirect_to "/welcome"
+    redirect_to welcome_path
   end
 
   def kitten
@@ -29,7 +29,7 @@ class PagesController < ApplicationController
       render :secrets
     else
       flash[:alert] = "Sorry, you're not authorized to see that page!"
-      redirect_to :welcome
+      redirect_to welcome_path
     end
   end
 
